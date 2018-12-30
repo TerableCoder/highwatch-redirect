1da00e171ee5ade4ef510f0e7670664e7b78ea96
Tera Coder <31596711+TerableCoder@users.noreply.github.com>
2018-12-30 02:57:19 -0500

    upgrade

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # highwatch-redirect
-A `tera-proxy` module that redirects the teleport location from the Vanguard merchant to the Banker next to the crafting area, when hitting Redeem button in Vanguard window.
+A `tera-proxy` module that redirects the teleport location from the Vanguard merchant to the South-West Banker, when hitting Redeem button in Vanguard window.
+
+From mophiu
+
+Upgraded by TerableCoder
 
 ## Usage
 ### `hw`
 - Toggle module on/off
+- Default is on
+
+### `hwtree`
+- Toggle treehouse banker on/off
 - Default is on
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const Vec3 = require('tera-vec3');
 
-module.exports = function HwRedirect(mod) {
-  const highwatchRedeem = new Vec3(22205, 4870, 6191);
-  const highwatchBanker = new Vec3(22438, 1605, 5857);
+module.exports = function HwRedirect(dispatch) {
+	const highwatchRedeem = new Vec3(22205, 4870, 6191);
+	const highwatchBanker = new Vec3(22438, 1605, 5857);
+	const treehouseBanker = new Vec3(19395, 4335, 6191);
 
-  let enabled = true;
+	let enabled = true;
+	let tree = true;
 
-  mod.hook('S_SPAWN_ME', 3, event => {
-    if (enabled && mod.game.me.zone == 7031 && event.loc.equals(highwatchRedeem)) {
-      event.loc = highwatchBanker;
-    }
-    return true;
-  })
+	dispatch.hook('S_SPAWN_ME', 3, event => {
+		if (enabled &&  dispatch.game.me.zone == 7031 && event.loc.equals(highwatchRedeem)) {
+			if(tree){
+				event.loc = treehouseBanker;
+			} else {
+				event.loc = highwatchBanker;
+			}
+		}
+		return true;
+	});
 
-  mod.command.add('hw', () => {
-    enabled = !enabled;
-    mod.command.message(enabled ? 'Highwatch Redirect enabled.' : 'Highwatch Redirect disabled.');
-  })
-  
-  this.destructor = function() {
-    mod.command.remove('hw');
-  }
+	dispatch.command.add('hw', () => {
+		enabled = !enabled;
+		command.message(enabled ? 'Highwatch Redirect enabled.' : 'Highwatch Redirect disabled.');
+	});
+	
+	dispatch.command.add('hwtree', () => {
+		tree = !tree;
+		command.message(tree ? 'treehouse Redirect enabled.' : 'treehouse Redirect disabled.');
+	});
+
+	//this.destructor = function() {
+	//	dispatch.command.remove('hw');
+	//};
 }
